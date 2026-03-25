e17334a4d5fa1110f684adc6eab30b0c498b3476
WT <go.to.another5@gmail.com>
2026-03-25 18:00:33 +0200

    0.0.2

--- ModMain.gd
+++ ModMain.gd
@@ -4,7 +4,7 @@ const MOD_PRIORITY = 0
 const MOD_NAME = "Funnel Titan"
 const MOD_VERSION_MAJOR = 0
 const MOD_VERSION_MINOR = 0
-const MOD_VERSION_BUGFIX = 1
+const MOD_VERSION_BUGFIX = 2
 const MOD_VERSION_METADATA = ""
 const MOD_IS_LIBRARY = false
 
