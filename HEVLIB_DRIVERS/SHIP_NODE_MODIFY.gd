482f5479fb5cc956db5138da4531ac457d2e01f1
WT <go.to.another5@gmail.com>
2026-04-05 16:40:57 +0300

    #12 Config to change front slots into high stress

---
+++ HEVLIB_DRIVERS/SHIP_NODE_MODIFY.gd
@@ -0,0 +1,31 @@
+extends Node
+
+const CHARYBDIS = {
+	"ship_name": "SHIP_AT225_CB",
+	"modifications": [
+		{
+			"config": {"id": "FunnelTitan", "section": "FUNNEL_TITAN_CONFIG_SECTION", "entry": "use_main_slots"},
+			"path": "weapon-left",
+			"property": "remove",
+			"value": true
+		},
+		{
+			"config": {"id": "FunnelTitan", "section": "FUNNEL_TITAN_CONFIG_SECTION", "entry": "use_main_slots"},
+			"path": "weapon-right",
+			"property": "remove",
+			"value": true
+		},
+		{
+			"config": {"id": "FunnelTitan", "section": "FUNNEL_TITAN_CONFIG_SECTION", "entry": "use_main_slots", "invert_config": true},
+			"path": "weapon-left2",
+			"property": "remove",
+			"value": true
+		},
+		{
+			"config": {"id": "FunnelTitan", "section": "FUNNEL_TITAN_CONFIG_SECTION", "entry": "use_main_slots", "invert_config": true},
+			"path": "weapon-right2",
+			"property": "remove",
+			"value": true
+		}
+	]
+}
