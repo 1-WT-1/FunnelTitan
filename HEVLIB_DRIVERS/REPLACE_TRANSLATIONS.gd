d09d82e1d340d84f0ba4e7a8fe7206122fd40d35
WT <go.to.another5@gmail.com>
2026-04-06 22:10:28 +0300

    v0.1.2

--- HEVLIB_DRIVERS/REPLACE_TRANSLATIONS.gd
+++ HEVLIB_DRIVERS/REPLACE_TRANSLATIONS.gd
@@ -1,7 +1,92 @@
 extends Node
 
+# This translation file is generated automatically
+# Do not modify anything directly, as this can break things for those working on them
+# Please use Translation Tracker to modify these yourself, and contact the mod author to implement them
+# https://github.com/rwqfsfasxc100/TranslationTracker/releases/latest
+
 const TRANSLATIONS = {
-	"file": {
-		"res://FunnelTitan/i18n/en.txt": "|"
+	"master_locale": "en",
+	"en": {
+		"MOD_CHARYBDIS_DESC": {
+			"string": "A K225 variant with a front triangle section hollowed out into a funnel.",
+			"version_hash": 3003134423
+		},
+		"MOD_CHARYBDIS_BRIEF": {
+			"string": "Funnel K225 variant",
+			"version_hash": 1197557350
+		},
+		"SHIP_AT225_CB": {
+			"string": "Antonoff-Titan K225 \"Charybdis\"",
+			"version_hash": 273492910
+		},
+		"SYSTEM_FUNNEL_GRINDERS": {
+			"string": "Funnel Grinders",
+			"version_hash": 581204491
+		},
+		"SHIP_AT225_CB_DESC": {
+			"string": "This specialized excavation variant modifies the K225 hull by removing the default excavator and the two forward docking bays. In their place is a large intake funnel protected by a simple metal mesh, allowing rocks to feed directly into internal grinders. To make physical room for the funnel and the internal grinder machinery, thrusters have been relocated outwards.",
+			"version_hash": 3316047655
+		},
+		"SHIP_AT225_CB_SPECS": {
+			"string": "Make: Antonoff-Titan Heavy Industries\\nLow-Stress Hardpoints: 2\\nDocking Bays: 4\\nCrew: 12\\nDry mass: 225,100 kg\\nCargo bay: 246 m^3\\nProcessed cargo storage (per mineral): 7,000 kg",
+			"version_hash": 3414729862
+		},
+		"FUNNEL_UPGRADE_SLOT_NAME": {
+			"string": "Funnel Upgrade Slot",
+			"version_hash": 2882107991
+		},
+		"SYSTEM_FUNNEL_GRINDERS_1": {
+			"string": "1x Grinder Pair",
+			"version_hash": 3979405957
+		},
+		"SYSTEM_FUNNEL_GRINDERS_1_DESC": {
+			"string": "Adds one pair of grinders to the funnel.",
+			"version_hash": 2494577308
+		},
+		"SYSTEM_FUNNEL_GRINDERS_1_SPECS": {
+			"string": "Mass: 2,000 kg",
+			"version_hash": 620391795
+		},
+		"SYSTEM_FUNNEL_GRINDERS_3": {
+			"string": "3x Grinder Pairs",
+			"version_hash": 2880997210
+		},
+		"SYSTEM_FUNNEL_GRINDERS_3_DESC": {
+			"string": "Adds three pairs of grinders to the funnel.",
+			"version_hash": 2098729349
+		},
+		"SYSTEM_FUNNEL_GRINDERS_3_SPECS": {
+			"string": "Mass: 6,000 kg",
+			"version_hash": 3590439159
+		},
+		"SYSTEM_FUNNEL_GRINDERS_5": {
+			"string": "5x Grinder Pairs",
+			"version_hash": 3290616604
+		},
+		"SYSTEM_FUNNEL_GRINDERS_5_DESC": {
+			"string": "Fills the funnel with five pairs of grinders.",
+			"version_hash": 3759373934
+		},
+		"SYSTEM_FUNNEL_GRINDERS_5_SPECS": {
+			"string": "Mass: 10,000 kg",
+			"version_hash": 3438383938
+		},
+		"SYSTEM_FUNNEL_GRINDERS_MANUAL": {
+			"string": "Toggled alongside the main excavator.\\nWhen active, the grinder assembly moves inward into the funnel.\\nCrushes incoming ringroinds.",
+			"version_hash": 1462107994
+		},
+		"FUNNEL_TITAN_CONFIG_SECTION": {
+			"string": "Funnel Titan Settings",
+			"version_hash": 2990543326
+		},
+		"FUNNEL_TITAN_CONFIG_MAIN_SLOTS": {
+			"string": "High-stress for Forward Mounts",
+			"version_hash": 1863662072
+		},
+		"FUNNEL_TITAN_CONFIG_MAIN_SLOTS_DESC": {
+			"string": "If enabled, forward mounts will be high-stress hardpoints instead of low-stress \nWARNING REMOVE EQUIPMENT IN THESE SLOTS BEFORE RESTARTING GAME\nRequires game restart",
+			"version_hash": 3828026086
+		}
 	}
 }
